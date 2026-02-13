97e2d0130b3600f273e46899ab79f22d1c42a03a
RedFeuer <danilabespyatov@gmail.com>
2026-02-13 19:55:14 +0300

    ADD: dependencies

--- app/src/main/java/com/example/central_app_ble/presentation/viewModel/UiState.kt
+++ app/src/main/java/com/example/central_app_ble/presentation/viewModel/UiState.kt
@@ -1,5 +1,8 @@
 package com.example.central_app_ble.presentation.viewModel
 
+import com.example.central_app_ble.domain.domainModel.BleDevice
+import com.example.central_app_ble.domain.domainModel.ConnectionState
+
 data class UiState(
     val selected: BleDevice? = null,
     val connectionState: ConnectionState = ConnectionState.Idle,
