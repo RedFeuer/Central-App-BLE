04fda2163a4cb6b4692a9fa5edffd229288e0f72
RedFeuer <danilabespyatov@gmail.com>
2026-02-16 20:37:40 +0300

    IMPROVE: moved permissions to shared to use both for Central and Peripheral devices

--- shared/src/main/kotlin/com/example/shared/BlePermissionGate.kt
+++ shared/src/main/kotlin/com/example/shared/BlePermissionGate.kt
@@ -1,26 +1,29 @@
-package com.example.central_app_ble.presentation.permissions
+package com.example.shared
 
-import android.Manifest
 import android.os.Build
 
+/* Central: BLUETOOTH_SCAN, BLUETOOTH_CONNECT
+* Peripheral: BLUETOOTH_ADVERTISE, BLUETOOTH_CONNECT */
 class BlePermissionGate(
+    private val requiredPerms31: Array<String>, // теперь подходит и для Central и для Peripheral
     private val request: (Array<String>) -> Unit, // лямбда для запроса разрешений
     private val isGranted: (String) -> Boolean, // лямбда для проверки, выдано ли конкретное разрешение
 ) {
     /* Обязательные permission'ы для Android 12 и выше */
-    private val blePerms31 = arrayOf(
-        Manifest.permission.BLUETOOTH_SCAN,
-        Manifest.permission.BLUETOOTH_CONNECT
-    )
+    /* было для CentralApp */
+//    private val blePerms31 = arrayOf(
+//        Manifest.permission.BLUETOOTH_SCAN,
+//        Manifest.permission.BLUETOOTH_CONNECT
+//    )
 
     fun ensurePermsOrRequest(): Boolean {
         if (Build.VERSION.SDK_INT < 31) return true // Android 11 и ниже
 
         /* проверка, что права выданы */
-        val ok = blePerms31.all(isGranted)
+        val ok = requiredPerms31.all(isGranted)
         if (ok) return true // Android 12 и выше
 
-        request(blePerms31) // запрос разрешений (вызов диалога)
+        request(requiredPerms31) // запрос разрешений (вызов диалога)
         return false // отложит событие pendingEvent = event и выполнит если дам разрешения
     }
 }
